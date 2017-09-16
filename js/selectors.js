93ca912618e781858840b3afaad25cf1c8099b52
Seth Barden <sethbarden@gmail.com>
2017-09-16 18:42:15 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -5,3 +5,15 @@ function paragraphSelector() {
 function lastImageSelector() {
   $('img:last')
 }
+
+function ninjaBabySelector() {
+  $('#baby-ninja')
+}
+
+function divSelector() {
+  $('div .pics')
+}
+
+function firstListItem() {
+  $('ul #pic-list')
+}
